ab9059716b6a30f02dd82bbfc638936c08e4a0ab
manl400 <movie.edit@yahoo.com>
2018-05-08 00:26:38 -0500

    Setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
