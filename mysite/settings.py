96b69ebb97b55d287709dc3e02df632aa9796e15
manl400 <movie.edit@yahoo.com>
2018-05-08 00:17:30 -0500

    Fixed Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 db_from_env = dj_database_url.config()
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
